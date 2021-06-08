95d8b9bd5b74c364204181d8f9e534d8b5e5f582
Michal Nazarewicz <mina86@mina86.com>
2021-06-08 18:21:30 +0200

    Add copyright header to each source file

--- src/matrix.rs
+++ src/matrix.rs
@@ -1,3 +1,19 @@
+/* This file is part of rgb_derivation crate.
+ * Copyright 2021 by Michał Nazarewicz <mina86@mina86.com>
+ *
+ * rgb_derivation crate is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or (at your
+ * option) any later version.
+ *
+ * rgb_derivation crate is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * rgb_derivation crate.  If not, see <http://www.gnu.org/licenses/>. */
+
 //! Functions for calculating RGB↔XYZ conversion matrices and performing basic
 //! matrix manipulation.
 //!
